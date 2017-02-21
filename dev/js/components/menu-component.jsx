20db2c552847a349aadb4a52441374d16c6a2f92
student-repo <repostudent@gmail.com>
2017-02-21 01:30:12 +0100

    News component - ready

--- dev/js/components/menu-component.jsx
+++ dev/js/components/menu-component.jsx
@@ -4,25 +4,13 @@ import {syncHistoryWithStore} from 'react-router-redux'
 import {Col, Row} from "pui-react-grids";
 import * as menuLabelCss from '../../scss/menu-label.css'
 import SmallScreenMenu from './menu-small-screen'
-import ReactResizeDetector from 'react-resize-detector';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {resizeWindowUpdate} from '../actions/resize-window-state-action'
 import { WindowResizeListener } from 'react-window-resize-listener';
 
 
-
-
-var divStyle = {
-    backgroundColor: '#333333',
-    top: '0px',
-    position: 'fixed',
-    zIndex: '100',
-    width: '105%',
-    height: '100px'
-};
-
-const style = {
+const imageStyle = {
     'maxWidth': '100%',
     'cursor': 'pointer',
     'maxHeight': '90px',
@@ -31,7 +19,7 @@ const style = {
 
 const menuModeChangeValue = 1100;
 
-const ss = {
+const menuStyle = {
     backgroundColor: '#333333',
     top: '0px',
     position: 'fixed',
@@ -40,17 +28,13 @@ const ss = {
 }
 
 
-const sss = {
-    textAlign: "right"
-}
-
 const MenuComponent = ({resizeWindowUpdate, windowWidth}) => (
     <Row>
         <WindowResizeListener onResize={windowSize => resizeWindowUpdate(windowSize.windowWidth)}/>
 
         {windowWidth > menuModeChangeValue ?
-            <Col md={4} style={ss}>
-                <img className="entryImage" src="Tennis-Logo-scale.png" alt="Unable to load image" style={style}/>
+            <Col md={4} style={menuStyle}>
+                <img className="entryImage" src="Tennis-Logo-scale.png" alt="Unable to load image" style={imageStyle}/>
                 <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("News")}>News</font>
                 <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("Reservation")}>Reservation</font>
                 <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("Tennis School")}>Tennis School</font>
@@ -59,32 +43,14 @@ const MenuComponent = ({resizeWindowUpdate, windowWidth}) => (
                 <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("Prices")}>Prices</font>
             </Col> :
             <div>
-                <Col md={4} style={ss}>
-                    <img className="entryImage" src="Tennis-Logo-scale.png" alt="Unable to load image" style={style}/>
+                <Col md={4} style={menuStyle}>
+                    <img className="entryImage" src="Tennis-Logo-scale.png" alt="Unable to load image" style={imageStyle}/>
                         <SmallScreenMenu />
                 </Col>
             </div>
             }
     </Row>
 );
-
-
-const style3 = {
-    color: 'grey',
-    fontWeight: 'bold',
-    cursor: 'pointer'
-};
-
-
-const descriptionStyle = {
-    fontFamily: 'Courier New',
-    color: '#939393',
-    padding: '6px',
-    borderRadius: '6px',
-     fontWeight: 'bold',
-    fontSize: '20px',
-    cursor: 'pointer'
-};
 
 function mapStateToProps(state) {
     return {
@@ -97,37 +63,3 @@ function matchDispatchToProps(dispatch){
 }
 
 export default connect(mapStateToProps, matchDispatchToProps)(MenuComponent);
-
-//
-// {
-//     windowWidth > menuModeChangeValue ? <div>
-//         <Col md={4}/>
-//         <Col md={1}>
-//             <br/>
-//             <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("News")}>News</font>
-//         </Col>
-//         <Col md={3}>
-//             <br/>
-//             <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("Reservation")}>Reservation</font>
-//         </Col>
-//         <Col md={3}>
-//             <br/>
-//             <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("Tennis School")}>Tennis School</font>
-//         </Col>
-//         <Col md={2}>
-//             <br/>
-//             <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("Gallery")}>Gallery</font>
-//         </Col>
-//         <Col md={2}>
-//             <br/>
-//             <font className="menu-labels" style={menuLabelCss} onClick={() => console.log("Contact")}>Contact</font>
-//         </Col>
-//         <Col md={3}>
-//             <br/>
-//             <font className="menu-labels" style={menuLabelCss} onClick={() => console.log(window.innerWidth)}>Prices</font>
-//         </Col>
-//     </div> :
-//         <Col md={3} style={sss}>
-//             <SmallScreenMenu/>
-//         </Col>
-// }
--- dev/js/components/news.jsx
+++ dev/js/components/news.jsx
@@ -4,6 +4,52 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {newsUpload} from '../actions/action-new-upload'
 import {Col, Row} from "pui-react-grids";
+import Lightbox from 'react-images'
+import Gallery from './gallery';
+
+
+const DEFAULT_IMAGES = [
+    { id: '1470619549108-b85c56fe5be8', caption: 'Photo by Alan Emery', orientation: 'square', useForDemo: true }
+];
+
+const newsStyle = {
+    fontFamily: 'Courier New',
+    padding: '6px',
+    borderRadius: '6px',
+    fontWeight: 'bold',
+    fontSize: '35px',
+    textAlign: 'center',
+    maxWidth: '100%',
+    color: 'black'
+};
+
+const titleStyle = {
+    fontFamily: 'Courier New',
+    padding: '6px',
+    borderRadius: '6px',
+    fontWeight: 'bold',
+    fontSize: '20px',
+    maxWidth: '100%',
+    color: '#696969',
+};
+
+const dateStyle = {
+    fontFamily: 'Courier New',
+    padding: '6px',
+    borderRadius: '6px',
+    fontSize: '10px',
+    maxWidth: '100%',
+    color: '#b3b3b3',
+};
+
+const contentStyle = {
+    fontFamily: 'Courier New',
+    padding: '6px',
+    borderRadius: '6px',
+    fontSize: '15px',
+    maxWidth: '100%',
+    color: '#8c8c8c',
+};
 
 const News = ({socket, newsUpload, news}) => {
     socket.on('INITIAL_DATA', function(data){
@@ -18,12 +64,48 @@ const News = ({socket, newsUpload, news}) => {
 
     return(
         <Row>
-            <h3>News</h3>
-            {news.map(key => {
-                return (
-                    <p key={key.NewsID}>{key.Content}</p>
-                )
-            })}
+            <Col md={1}/>
+            <Col md={22} style={{maxWidth: '70vw', float: 'none', margin: '0 auto'}}>
+                <Row style={{textAlign: 'center'}}>
+                    <br/>
+                    <font style={newsStyle}>News</font>
+                </Row>
+                {news.map(key => {
+                    return (
+                        <Row key={key.NewsID + Math.random()}>
+                            <Row key={key.NewsID + Math.random()}>
+                                <Col md={13}>
+                                    <font style={titleStyle} key={key.NewsID + Math.random()}>{key.Title}</font>
+                                </Col>
+                            </Row>
+                            <Row key={key.NewsID + Math.random()}>
+                                <Col md={13}>
+                                    <font style={dateStyle} key={key.NewsID + Math.random()}>{key.Date}</font>
+                                </Col>
+                            </Row>
+                            <Row key={key.NewsID + Math.random()}>
+                                <Col md={13}>
+                                    <font style={contentStyle} key={key.NewsID + Math.random()}>{key.Content}</font>
+                                </Col>
+                                <div>
+                                    <Gallery images={DEFAULT_IMAGES.map(({ caption, id, orientation, useForDemo }) => ({
+                                        src: 'tennis-banner-3.jpg',
+                                        thumbnail: `tennis-banner-3.jpg`,
+                                        caption,
+                                        orientation,
+                                        useForDemo,
+                                    }))} />
+                                </div>
+                            </Row>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </Row>
+
+                    )
+                })}
+            </Col>
+            <Col md={1}/>
         </Row>
     )}
 
